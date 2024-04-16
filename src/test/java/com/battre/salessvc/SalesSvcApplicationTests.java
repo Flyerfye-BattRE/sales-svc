c15244ec2d8150ff995206bd4a2d50dd278486ca
Issa Beekun <goodflyerfye@gmail.com>
2024-04-16 08:02:19 -0700

    Initial commit

---
+++ src/test/java/com/battre/salessvc/SalesSvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.battre.salessvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesSvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
